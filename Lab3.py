ddf52a7705e89f262980ae0cff4191334e783ea5
MihailCiubotaru <158076885+MihailCiubotaru@users.noreply.github.com>
2024-04-08 22:18:09 +0300

    Update Lab3.py
    toate functiile ok

--- Lab3.py
+++ Lab3.py
@@ -2,125 +2,217 @@ import os
 import time
 import threading
 import json
+import struct
+import datetime
 
-cale_director = "E:\TEMPORARE\OOP_Mihai"
-cale_snapshot = os.path.join(cale_director, "SnapShots")
+FOLDER_PATH = "E:\TEMPORARE\MyGithub"
+SnapListName = "CommitList.json"
+TreePos = -1
+SNAPSHOT_FOLDER = os.path.join(FOLDER_PATH, "SnapShots")
+SNAPSHOT_TIME = None
+CurrentSnapshot = []
+CurrentCommit = []
+LastSnapshot = []
+FILE_CURRENT_LIST = []
+FILE_NAME_LIST = os.listdir(FOLDER_PATH)
 
-timp_snapshot = None
-snapshot_curent = []
-ultimul_commit = []
-snapshot_anterior = []
-lista_fisiere_curenta = []
-lista_fisiere = os.listdir(cale_director)
+if not os.path.exists(SNAPSHOT_FOLDER):
+    os.makedirs(SNAPSHOT_FOLDER)
 
-class DateFisier:
-    def __init__(self, nume_fisier, dimensiune, creat, modificat):
-        self.nume_fisier = nume_fisier
-        self.dimensiune = dimensiune
-        self.creat = creat
-        self.modificat = modificat
+class FileInfo:
+    def __init__(self, filename, size, created, modified,specific):
+        self.filename = filename
+        self.size = size
+        self.created = created
+        self.modified = modified
+        self.specific = specific
 
-def preia_date_fisier(nume_fisier):
-    cale_fisier = os.path.join(cale_director, nume_fisier)
-    if not os.path.exists(cale_fisier):
-        print(f"Fisierul '{nume_fisier}' nu a fost gasit.")
+class Snapshot:
+    def __init__(self):
+        self.time = time.time()
+        self.file_info_list = []
+
+class SnapListEncoder(json.JSONEncoder):
+    def default(self, obj):
+        entry = dict(obj.__dict__)
+        entry['__class__'] = obj.__class__.__name__
+        return entry
+
+class SnapListDecoder(json.JSONDecoder):
+    def __init__(self):
+        json.JSONDecoder.__init__(self, object_hook=self.dict_to_object)
+
+    def dict_to_object(self, dictionary):
+        if dictionary.get("__class__") == "FileInfo":
+            obj = FileInfo.__new__(FileInfo)
+        elif dictionary.get("__class__") == "Snapshot":
+            obj = Snapshot.__new__(Snapshot)
+        else:
+            return dictionary
+
+        for key, value in dictionary.items():
+            if key != '__class__':
+                setattr(obj, key, value)
+        return obj
+
+def get_meta_info(filename):
+    if filename == "SnapShots":
+        return None
+    filepath = os.path.join(FOLDER_PATH, filename)
+    
+    if not os.path.exists(filepath):
+        print(f"File '{filename}' not found.")
         return None
 
-    stat = os.stat(cale_fisier)
-    date_fisier = DateFisier(
-        nume_fisier=nume_fisier,
-        dimensiune=stat.st_size,
-        creat=time.ctime(stat.st_ctime),
-        modificat=time.ctime(stat.st_mtime)
+    stat = os.stat(filepath)
+    spec = ""
+    
+    if filename.lower().endswith((".png", ".jpg")):
+        with open(filepath, "rb") as img_file:
+            img_data = img_file.read(24)
+            width,height = struct.unpack(">II", img_data[16:24])
+        spec = str(width) + ' X ' + str(height)
+        #spec = "Imagine"
+    elif filename.lower().endswith((".txt")):
+        with open(filepath, "r") as file:
+            read = file.read()
+            spec = str(len(read.splitlines())) + " Linii " + str(len(read.split())) + " cuvinte " + str(len(read)) + " Caractere"
+    elif filename.lower().endswith((".py")):
+        with open(filepath, "r") as file:
+            read = file.read()
+            spec = str(len(read.splitlines())) + " Linii " + str(read.lower().count("class")) + " Classes " + str(read.lower().count("def")) + " Methodes"
+
+    file_info = FileInfo(
+        filename=filename,
+        size=stat.st_size,
+        created=time.ctime(stat.st_ctime),
+        modified=time.ctime(stat.st_mtime),
+        specific= spec
     )
-    return date_fisier
+    return file_info
 
-def gaseste_fisiere():
-    lista_date_fisiere = []
-    for nume_fisier in lista_fisiere:
-        date_fisier = preia_date_fisier(nume_fisier)
-        if date_fisier:
-            lista_date_fisiere.append(date_fisier)
-    return lista_date_fisiere
+def Search():
+    file_info_list = []  # a list of FileInfo objects
+    file_name_list = os.listdir(FOLDER_PATH)
+    
+    for file_name in file_name_list:
+        file_info = get_meta_info(file_name)
+        if file_info:  # Check if get_meta_info() returned a valid FileInfo object
+            file_info_list.append(file_info)
+#    RETURN_LIST = file_info_list
+    return file_info_list
 
-def salveaza_snapshot(nume_snapshot):
-    global snapshot_curent
-
-    snapshot_curent = gaseste_fisiere()
-    date_snapshot = json.dumps([ob.__dict__ for ob in snapshot_curent], indent=2)
-
-    cale_fisier_snapshot = os.path.join(cale_snapshot, nume_snapshot)
-    cale_fisier_timp_snapshot = os.path.join(cale_snapshot, "Creat.txt")
-
-    snap = time.time()
-    with open(cale_fisier_timp_snapshot, "w") as sTime:
-        sTime.write(f"{time.ctime(snap)}\n")
-
-    with open(cale_fisier_snapshot, "w") as fisier_snapshot:
-        fisier_snapshot.write(date_snapshot)
-        print(f"Snapshot salvat in {nume_snapshot}")
-
-def citeste_snapshot(nume_snapshot):
-    global timp_snapshot
-    cale_fisier_snapshot = os.path.join(cale_snapshot, nume_snapshot)
-    cale_fisier_timp_snapshot = os.path.join(cale_snapshot, "Creat.txt")
-
-    with open(cale_fisier_timp_snapshot, 'r') as t:
-        timp_snapshot = t.read().strip()
-
-    with open(cale_fisier_snapshot, 'r') as fisier_json:
-        date = json.load(fisier_json)
-
-    lista_date_fisiere = []
-    for item in date:
-        date_fisier = DateFisier(
-            nume_fisier=item['nume_fisier'],
-            dimensiune=item['dimensiune'],
-            creat=item['creat'],
-            modificat=item['modificat']
-        )
-        lista_date_fisiere.append(date_fisier)
-    return lista_date_fisiere
-
-def afiseaza_date_fisier(lista_fisiere):
-    if lista_fisiere:
-        print("Lista de fisiere nu este goala:")
+def print_file_info(file_list):
+    if file_list:
+        for file in file_list:
+            print(f"File: {file.filename}    {file.size} bytes  Ct: {file.created}    Md: {file.modified}   Specific: {file.specific}")
     else:
-        print("Lista de fisiere este goala")
+        print("empty List")
 
-    for fisier in lista_fisiere:
-        print(f"Fisier: {fisier.nume_fisier}\t\t{fisier.dimensiune} bytes\t\tCreat: {fisier.creat}\t\tModificat: {fisier.modificat}")
-def verifica_modificari(lista_prima, lista_ultima):
-    nume_fisiere_ultima = {date_fisier.nume_fisier for date_fisier in lista_ultima}
-
-    for date_fisier_curent in lista_prima:
-        if date_fisier_curent.nume_fisier not in nume_fisiere_ultima:
-            print(f"{date_fisier_curent.nume_fisier} - Fisier nou")
+def print_file_info_1(file_list,name):
+    if file_list:
+        for file in file_list:
+            if name == file.filename:
+                print(f"File: {file.filename}    {file.size} bytes  Ct: {file.created}    Md: {file.modified}   Specific: {file.specific}")
+    else:
+        print("empty List")
+def check_modified_objects(FirstList, LastList):
+    last_filenames = {file_info.filename for file_info in LastList}
+    #print("Check 5 Second")
+    
+    for current_file_info in FirstList:
+        if current_file_info.filename not in last_filenames:
+            print(f"{current_file_info.filename} - New file")
         else:
-            date_fisier_ultim = next(date_fisier for date_fisier in lista_ultima if date_fisier.nume_fisier == date_fisier_curent.nume_fisier)
-            if date_fisier_curent.modificat != date_fisier_ultim.modificat or date_fisier_curent.dimensiune != date_fisier_ultim.dimensiune:
-                print(f"{date_fisier_curent.nume_fisier} - A fost modificat")
+            last_file_info = next(file_info for file_info in LastList if file_info.filename == current_file_info.filename)
+            if current_file_info.modified != last_file_info.modified or current_file_info.size != last_file_info.size or current_file_info.specific !=last_file_info.specific:
+                print(f"{current_file_info.filename} - Has been modified")
 
-    for date_fisier_ultim in lista_ultima:
-        if date_fisier_ultim.nume_fisier not in {date_fisier.nume_fisier for date_fisier in lista_prima}:
-            print(f"{date_fisier_ultim.nume_fisier} - A fost șters")
+    # Check deleted files
+    for last_file_info in LastList:
+        if last_file_info.filename not in {file_info.filename for file_info in FirstList}:
+            print(f"{last_file_info.filename} - Has been deleted")
 
-while True:
-    ultimul_commit = snapshot_anterior = snapshot_curent = citeste_snapshot("Snapshot.json")
-    thread = threading.Thread(target=repeta_verificare, daemon=True)
-    thread.start()
+def repeat_check():
+    global LastSnapshot, CurrentSnapshot  # Declare them as global
+    while True:
+        LastSnapshot = CurrentSnapshot
+        CurrentSnapshot = Search()  # Replace with your actual Search function
+        check_modified_objects(CurrentSnapshot, LastSnapshot)
+        time.sleep(5)
 
-    actiune = input("Introduceți acțiunea (commit | info | status | exit): ").strip().lower()
+def SaveAllCommits(tree):
+    with open(os.path.join(SNAPSHOT_FOLDER,SnapListName), 'w') as file:
+        json.dump(tree, file, cls=SnapListEncoder)
 
-    if actiune == "commit":
-        salveaza_snapshot("Snapshot.json")
-    elif actiune == "info":
-        afiseaza_date_fisier(snapshot_curent)
-    elif actiune == "status":
-        snapshot_curent = gaseste_fisiere()
-        verifica_modificari(snapshot_curent, ultimul_commit)
-    elif actiune == "exit":
-        print("Programul se închide.")
+def LoadAllCommits():
+    with open(os.path.join(SNAPSHOT_FOLDER,SnapListName), 'r') as file:
+        tree = json.load(file, cls=SnapListDecoder)
+    return tree
+
+
+tree = []
+try:
+    tree = LoadAllCommits()
+    CurrentCommit = LastSnapshot = CurrentSnapshot = tree[TreePos].file_info_list
+    SNAPSHOT_TIME = tree[TreePos].time
+except:
+    print("Warning: the snapshots couldnt be loaded, prease use 'commit' to save a snapshot")
+thread = threading.Thread(target=repeat_check, daemon=True)
+thread.start()
+
+while True:  # main
+    action = input("Enter action (commit | info | status | exit): ").strip().lower()
+    
+    if action.startswith("commit"):
+        words = action.split()
+        if len(words) > 1:
+            other_word = words[1]
+            if other_word == "last" or other_word == "l":
+                TreePos -=1
+                CurrentCommit = tree[TreePos].file_info_list
+                NAPSHOT_TIME = tree[TreePos].time
+                dt_object = datetime.datetime.fromtimestamp(SNAPSHOT_TIME)
+                print("Loaded previous commit on the date: " + dt_object.strftime("%Y-%m-%d %H:%M:%S"))
+            elif other_word == "next" or other_word == "n":
+                TreePos +=1
+                if  TreePos < 0:
+                    CurrentCommit = tree[TreePos].file_info_list
+                    SNAPSHOT_TIME = tree[TreePos].time
+                    dt_object = datetime.datetime.fromtimestamp(SNAPSHOT_TIME)
+                    print("Loaded next commit on the date: " + dt_object.strftime("%Y-%m-%d %H:%M:%S"))
+                else:
+                    print("stop,you are at the last saved commit")
+        else:
+            T = Snapshot()
+            CurrentCommit = CurrentSnapshot = Search()
+            T.file_info_list = CurrentCommit
+            tree.append(T)
+            SaveAllCommits(tree)
+    
+    elif action.startswith("info"):
+        words = action.split()
+        if len(words) > 1:
+            other_word = words[1]
+            if other_word == "commit" or other_word == "c" :
+                print_file_info(CurrentCommit)
+            else:
+                print_file_info_1(CurrentSnapshot,other_word)
+        else:
+            print_file_info(CurrentSnapshot)
+
+    elif action.startswith("status"): # status between CurrentSnapshot and last Commit
+        words = action.split()
+        if len(words) > 1:
+            other_word = words[1]
+            if other_word == "commit" or other_word == "c":
+                check_modified_objects(CurrentSnapshot,CurrentCommit)
+        else:
+            CurrentSnapshot = Search()
+            check_modified_objects(CurrentSnapshot,LastSnapshot)
+
+    elif action == "exit":
+        print("Exiting program.")
         break
     else:
-        print("Acțiune invalidă. Acțiunile disponibile sunt: (commit | info | status | exit)")
+        print("Invalid action. Available actions: (commit | info | status | exit)")
